2aa1e34a8d4f605cf9e365fa2e2ea2a3385e1fb2
dre1930 <elishaekpo66@gmail.com>
2024-12-04 06:31:29 -0800

    Create text.js

---
+++ text.js
@@ -0,0 +1,20 @@
+alert("welcome to Java");
+
+let myName = 'Dre';
+let myAge = 20; //Replace with your age
+
+if (myAge >= 18) {
+    console.log(myName + ", you are eligible to vote")
+}else {
+    console.log(myName + ", you are not eligible to vote")
+}
+
+let Balance = 10000;
+let Withdrawal = 5000
+
+if (Withdrawal <= 10000) {
+    console.log("Transaction successful")
+}else{
+    console.log("Transaction declined")
+}
+
