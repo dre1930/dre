81d9af950948cc408dd3af42b43f6b6f0f5048f4
dre1930 <elishaekpo66@gmail.com>
2025-01-10 04:29:30 -0800

    Update text.js

--- text.js
+++ text.js
@@ -1,6 +1,6 @@
 alert("welcome to Java");
 
-let myName = 'Dre';
+/*let myName = 'Dre';
 let myAge = 20; //Replace with your age
 
 if (myAge >= 18) {
@@ -16,9 +16,16 @@ if (Withdrawal <= 10000) {
     console.log("Transaction successful")
 }else{
     console.log("Transaction declined")
-}
+}*/
 
-for (let i = 1; i < 100; i+=2) {
+
+for (let i = 1; i < 100; i+=2) 
     console.log(i)
 
-}
+
+    const pinCode=[]
+    for (let i = 0; i <= 999; i++) {
+        let pin = i.toString().padStart(3, '0');
+        pinCode.push(pin);
+    }
+    console.log(pinCode);
