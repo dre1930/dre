09569c8e675faec4cc9e9676d9f3ff90d22b8adc
dre1930 <elishaekpo66@gmail.com>
2025-05-02 13:30:59 +0100

    lm;m;

---
+++ scope.js/scope.js
@@ -0,0 +1,9 @@
+//async function mydata () {
+   // try{
+       // let res = await fetch ('https://fakestoreapi.com/products/1')
+       // let data = await res.json()
+       // return data
+   // }catch {error} {
+     //   console.log('error occured ${error}')
+   // }
+//}
